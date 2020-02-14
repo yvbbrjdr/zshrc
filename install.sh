d4e04dd6f0b30577cf8ad8483d048140cf30b3da
yvbbrjdr <xyyqzy@hotmail.com>
2020-02-13 20:41:09 -0800

    install

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 DEST="$ZSH/custom"
+CUSTOM_FILE="yvzshrc_custom.zsh"
+OTHER_FILES="yvzshrc_apt.zsh yvzshrc_global.zsh yvzshrc_pacman.zsh yvzshrc_systemd.zsh yvzshrc_yay.zsh"
 cd "$(dirname "$0")"
 if [[ -w "$DEST" ]]; then
-    cp *.zsh "$DEST"
+    cp $OTHER_FILES "$DEST"
+    if [[ ! -f "$DEST/$CUSTOM_FILE"  ]]; then
+        cp "$CUSTOM_FILE" "$DEST"
+    fi
 else
-    sudo cp *.zsh "$DEST"
+    sudo cp $OTHER_FILES "$DEST"
+    if [[ ! -f "$DEST/$CUSTOM_FILE"  ]]; then
+        sudo cp "$CUSTOM_FILE" "$DEST"
+    fi
 fi
